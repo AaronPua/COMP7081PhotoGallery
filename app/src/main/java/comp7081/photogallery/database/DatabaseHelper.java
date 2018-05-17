f0a1b0f63676609137d442d97a4f515ecfa074be
Aaron Pua <aaronpua@hotmail.com>
2018-05-16 19:13:27 -0700

    Fix database lock issues.

--- app/src/main/java/comp7081/photogallery/database/DatabaseHelper.java
+++ app/src/main/java/comp7081/photogallery/database/DatabaseHelper.java
@@ -45,7 +45,6 @@ public class DatabaseHelper extends SQLiteOpenHelper implements DatabaseStorageI
     private static final String KEY_COUNTRY = "country";
     private static final String KEY_POSTAL = "postal_code";
 
-
     // Photos Table Create Statement
     private static final String CREATE_TABLE_PHOTOS = "CREATE TABLE " + DB_TABLE_PHOTOS + "("+
             KEY_TABLE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
@@ -69,9 +68,12 @@ public class DatabaseHelper extends SQLiteOpenHelper implements DatabaseStorageI
 
     private static final String PRAGMA_FOREIGN_KEY = "PRAGMA foreign_keys = ON;";
 
+    SQLiteDatabase database;
+
     // Constructor
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        database = this.getWritableDatabase();
     }
 
     @Override
@@ -93,7 +95,7 @@ public class DatabaseHelper extends SQLiteOpenHelper implements DatabaseStorageI
     }
 
     public void addPhotoEntry(Photo photo) throws SQLiteException {
-        SQLiteDatabase database = this.getWritableDatabase();
+        //SQLiteDatabase database = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put(KEY_IMAGE, photo.getImage());
         cv.put(KEY_NAME, photo.getName());
@@ -101,11 +103,11 @@ public class DatabaseHelper extends SQLiteOpenHelper implements DatabaseStorageI
         cv.put(KEY_CAPTION, photo.getCaption());
 
         database.insert(DB_TABLE_PHOTOS, null, cv);
-        database.close();
+        //database.close();
     }
 
     public ArrayList<Photo> getAllPhotos() {
-        SQLiteDatabase database = this.getReadableDatabase();
+        //SQLiteDatabase database = this.getReadableDatabase();
         String selectQuery =  "SELECT * FROM " + DB_TABLE_PHOTOS;
 
         ArrayList<Photo> photoArrayList = new ArrayList<Photo>();
@@ -140,12 +142,12 @@ public class DatabaseHelper extends SQLiteOpenHelper implements DatabaseStorageI
         catch (SQLiteException ex) {
             ex.printStackTrace();
         }
-        database.close();
+        //database.close();
         return null;
     }
 
     public ArrayList<Photo> getPhotosByDate(String startDate, String endDate) {
-        SQLiteDatabase database = this.getReadableDatabase();
+        //SQLiteDatabase database = this.getReadableDatabase();
         String selectQuery =  "SELECT * " + " FROM " + DB_TABLE_PHOTOS +
                 " WHERE " + KEY_DATE + " BETWEEN " + "?" + " AND " + "?";
         String[] selectionArgs = new String[]{startDate, endDate};
@@ -181,12 +183,12 @@ public class DatabaseHelper extends SQLiteOpenHelper implements DatabaseStorageI
         catch (SQLiteException ex) {
             ex.printStackTrace();
         }
-        database.close();
+        //database.close();
         return null;
     }
 
     public ArrayList<Photo> getPhotosByCaption(String caption) {
-        SQLiteDatabase database = this.getReadableDatabase();
+        //SQLiteDatabase database = this.getReadableDatabase();
         String selectQuery =  "SELECT * " + " FROM " + DB_TABLE_PHOTOS +
                 " WHERE " + KEY_CAPTION + "= ?";
         String[] selectionArgs = new String[]{caption};
@@ -222,12 +224,12 @@ public class DatabaseHelper extends SQLiteOpenHelper implements DatabaseStorageI
         catch (SQLiteException ex) {
             ex.printStackTrace();
         }
-        database.close();
+        //database.close();
         return null;
     }
 
     public void updateCaption(int id, String caption) {
-        SQLiteDatabase database = this.getWritableDatabase();
+        //SQLiteDatabase database = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put(KEY_CAPTION, caption);
 
@@ -235,11 +237,11 @@ public class DatabaseHelper extends SQLiteOpenHelper implements DatabaseStorageI
         id = id + 1;
 
         database.update(DB_TABLE_PHOTOS, cv, KEY_TABLE_ID + " = ?", new String[] {String.valueOf(id)});
-        database.close();
+        //database.close();
     }
 
     public void addLocationForPhoto(Photo photo, LocationInfo locationInfo) throws SQLiteException {
-        SQLiteDatabase database = this.getWritableDatabase();
+        //SQLiteDatabase database = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
         cv.put(KEY_PHOTO_NAME, photo.getName());
         cv.put(KEY_LATITUDE, locationInfo.getLatitude());
@@ -251,11 +253,11 @@ public class DatabaseHelper extends SQLiteOpenHelper implements DatabaseStorageI
         cv.put(KEY_POSTAL, locationInfo.getPostalCode());
 
         database.insert(DB_TABLE_LOCATION, null, cv);
-        database.close();
+        //database.close();
     }
 
     public ArrayList<Photo> getPhotosByLatLong(String latitude, String longitude) {
-        SQLiteDatabase database = this.getReadableDatabase();
+        //SQLiteDatabase database = this.getReadableDatabase();
         String selectQuery =  "SELECT * " + " FROM " + DB_TABLE_PHOTOS +
                 " JOIN " + DB_TABLE_LOCATION + " ON " + DB_TABLE_PHOTOS + "." + KEY_NAME +
                 " = " + DB_TABLE_LOCATION + "." + KEY_PHOTO_NAME +
@@ -299,7 +301,7 @@ public class DatabaseHelper extends SQLiteOpenHelper implements DatabaseStorageI
         catch (SQLiteException ex) {
             ex.printStackTrace();
         }
-        database.close();
+        //database.close();
         return null;
     }
 }
